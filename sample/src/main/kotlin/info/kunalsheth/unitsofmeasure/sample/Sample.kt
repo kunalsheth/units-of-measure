5038e7155b50ab7dca2162a26c676d3edcdb51f6
Kunal Sheth <kunal@kunalsheth.info>
2017-11-10 20:42:13 -0800

    Updated Sample

--- sample/src/main/kotlin/info/kunalsheth/unitsofmeasure/sample/Sample.kt
+++ sample/src/main/kotlin/info/kunalsheth/unitsofmeasure/sample/Sample.kt
@@ -13,18 +13,40 @@ import info.kunalsheth.unitsofmeasure.generated.*
                 ),
                 Relation(Dimension(L = 1), Dimension(T = 1)),
                 Relation(Dimension(L = 1, T = -1), Dimension(T = 1))
+        ),
+        quantities = arrayOf(
+                Quantity("FooQuantity", Dimension(1, 2, 3, 4, 5, 6, 7))
+        ),
+        unitsOfMeasure = arrayOf(
+                UnitOfMeasure("FooUnit", 2.718, Dimension(1, 2, 3, 4, 5, 6, 7)),
+                UnitOfMeasure("BarUnit", 3.141, Dimension(1, 2, 3, 4, 5, 6, 7))
         )
 )
 fun main(args: Array<String>) {
-    val distance = 15.Feet
-    val time = 3.14.Seconds
-    val speed /*: Velocity*/ = distance / time
+
+    // Available using only `generateCommonUnits` and `relationships`
+
+    val distance: Length = 15.Feet
+    val time: Time = 3.14.Seconds
+    val speed: Velocity = distance / time
     println(speed.KilometersPerHour)
 
-    val acceleration /*: Acceleration*/ = speed / time
+    val acceleration: Acceleration = speed / time
     println(acceleration.MetersPerSecondSquared)
 
-    val v = 12.Volts
-    val i = 50.Amperes
-    val r /*: ElectricalResistance*/ = v / i
+    val v: ElectricPotential = 12.Volts
+    val i: ElectricCurrent = 50.Amperes
+    val r: ElectricalResistance = v / i
+
+
+    // Available using `quantities` and `unitsOfMeasure`
+
+    val k1: FooQuantity = 9000.FooUnit
+    val k2: FooQuantity = 9000.BarUnit
+    val k3: FooQuantity = k1 - k2
+    println(k1.BarUnit) // false: same number, different units
+    println(k2.FooUnit)
+
+    println(k3.FooUnit) // != 0: same numbers, different units
+    println(k3.BarUnit) // != 0: same numbers, different units
 }
