c6e7fcb820c442597edaf7887c49de1d0b678ef3
Kunal Sheth <kunal@kunalsheth.info>
2017-11-12 01:27:24 -0800

    Added sample for custom units and quantities

--- sample/src/main/kotlin/info/kunalsheth/unitsofmeasure/sample/Sample.kt
+++ sample/src/main/kotlin/info/kunalsheth/unitsofmeasure/sample/Sample.kt
@@ -14,7 +14,7 @@ typealias D = Dimension
                 R(D(L = 1), D(T = 1)) // generates Velocity * Time & Len / Time (amongst others)
         )
 )
-fun main(args: Array<String>) {
+fun simple() {
     val mass1 = 3.Kilograms
     val mass2 = 14.Ounces
     val sum = mass1 + mass2
@@ -38,4 +38,58 @@ fun main(args: Array<String>) {
     assert(distance in 29.UsMiles..30.UsMiles)
     assert(distance in 30.UsMiles..29.UsMiles) // this works too
     assert(aBitFaster in speed..(speed + 4.KilometersPerHour))
+}
+
+
+@Schema( // If you have any ideas on how I can make this less verbose, please tell me!
+        relationships = arrayOf(
+                R(
+                        D(M = 2, I = 4, N = 6),
+                        D(L = 1, T = 3, Theta = 5, J = 7)
+                )
+        ),
+        quantities = arrayOf(
+                // generates "typealias `Foo` = `M²⋅I⁴⋅N⁶`"
+                Quantity("Foo", D(M = 2, I = 4, N = 6)),
+                // generates "typealias `Bar` = `L⋅T³⋅Θ⁵⋅J⁷`"
+                Quantity("Bar", D(L = 1, T = 3, Theta = 5, J = 7))
+        ),
+        unitsOfMeasure = arrayOf(
+                UnitOfMeasure(
+                        name = "Kunals",
+                        factorToSI = 3.141, // 1 Kunal * 3.141 = 1 M²⋅I⁴⋅N⁶
+                        dimension = D(M = 2, I = 4, N = 6)
+                ),
+                UnitOfMeasure(
+                        "Sheths",
+                        2.718, // 1 Sheth * 2.718 = 1 L⋅T³⋅Θ⁵⋅J⁷
+                        D(L = 1, T = 3, Theta = 5, J = 7)
+                ),
+                UnitOfMeasure(
+                        "MegaKunals", // A more elegant prefix solution is coming soon!
+                        3.141 * 1_000_000,
+                        D(M = 2, I = 4, N = 6)
+                )
+        )
+)
+fun custom() {
+    val a: Foo = 17.Kunals
+    val b: Foo = 0.01.MegaKunals
+    val sum: Foo = a + b
+
+    assert(sum in 10_000.Kunals..10_100.Kunals)
+
+
+    val kunal = 846.Kunals
+    val sheth = 864.Sheths
+    val quotient: `L⁻¹⋅M²⋅T⁻³⋅I⁴⋅Θ⁻⁵⋅N⁶⋅J⁻⁷` = kunal / sheth
+
+    // this is no unit of measure defined for L⁻¹⋅M²⋅T⁻³⋅I⁴⋅Θ⁻⁵⋅N⁶⋅J⁻⁷
+    // we are forced to use `it.siValue`
+    assert(quotient.siValue in 1..2)
+}
+
+fun main(args: Array<String>) {
+    simple()
+    custom()
 }
