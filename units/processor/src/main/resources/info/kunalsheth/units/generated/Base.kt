6320116235f7c60ebce2bfe4f4d463d2fc6704bb
Kunal Sheth <kunal@kunalsheth.info>
2018-02-11 12:36:46 -0800

    removed unnecessary generics from Base

--- units/processor/src/main/resources/info/kunalsheth/units/generated/Base.kt
+++ units/processor/src/main/resources/info/kunalsheth/units/generated/Base.kt
@@ -46,50 +46,50 @@ private inline fun <reified Q : Quan<Q>> Q.eq(that: Any?) = when (that) {
 
 private val Number.d get() = toDouble()
 
-fun <N : Number, Q : Quan<Q>> N.exa(f: Number.() -> Q) = f(d * 1E18)
-fun <N : Number, Q : Quan<Q>> Q.exa(f: Q.() -> N) = f().d * 1E-18
+fun <Q : Quan<Q>> Number.exa(f: Double.() -> Q) = f(d * 1E18)
+fun <Q : Quan<Q>> Q.exa(f: Q.() -> Double) = f() * 1E-18
 
-fun <N : Number, Q : Quan<Q>> N.peta(f: Number.() -> Q) = f(d * 1E15)
-fun <N : Number, Q : Quan<Q>> Q.peta(f: Q.() -> N) = f().d * 1E-15
+fun <Q : Quan<Q>> Number.peta(f: Double.() -> Q) = f(d * 1E15)
+fun <Q : Quan<Q>> Q.peta(f: Q.() -> Double) = f() * 1E-15
 
-fun <N : Number, Q : Quan<Q>> N.tera(f: Number.() -> Q) = f(d * 1E12)
-fun <N : Number, Q : Quan<Q>> Q.tera(f: Q.() -> N) = f().d * 1E-12
+fun <Q : Quan<Q>> Number.tera(f: Double.() -> Q) = f(d * 1E12)
+fun <Q : Quan<Q>> Q.tera(f: Q.() -> Double) = f() * 1E-12
 
-fun <N : Number, Q : Quan<Q>> N.giga(f: Number.() -> Q) = f(d * 1E9)
-fun <N : Number, Q : Quan<Q>> Q.giga(f: Q.() -> N) = f().d * 1E-9
+fun <Q : Quan<Q>> Number.giga(f: Double.() -> Q) = f(d * 1E9)
+fun <Q : Quan<Q>> Q.giga(f: Q.() -> Double) = f() * 1E-9
 
-fun <N : Number, Q : Quan<Q>> N.mega(f: Number.() -> Q) = f(d * 1E6)
-fun <N : Number, Q : Quan<Q>> Q.mega(f: Q.() -> N) = f().d * 1E-6
+fun <Q : Quan<Q>> Number.mega(f: Double.() -> Q) = f(d * 1E6)
+fun <Q : Quan<Q>> Q.mega(f: Q.() -> Double) = f() * 1E-6
 
-fun <N : Number, Q : Quan<Q>> N.kilo(f: Number.() -> Q) = f(d * 1E3)
-fun <N : Number, Q : Quan<Q>> Q.kilo(f: Q.() -> N) = f().d * 1E-3
+fun <Q : Quan<Q>> Number.kilo(f: Double.() -> Q) = f(d * 1E3)
+fun <Q : Quan<Q>> Q.kilo(f: Q.() -> Double) = f() * 1E-3
 
-fun <N : Number, Q : Quan<Q>> N.hecto(f: Number.() -> Q) = f(d * 1E2)
-fun <N : Number, Q : Quan<Q>> Q.hecto(f: Q.() -> N) = f().d * 1E-2
+fun <Q : Quan<Q>> Number.hecto(f: Double.() -> Q) = f(d * 1E2)
+fun <Q : Quan<Q>> Q.hecto(f: Q.() -> Double) = f() * 1E-2
 
-fun <N : Number, Q : Quan<Q>> N.deca(f: Number.() -> Q) = f(d * 1E1)
-fun <N : Number, Q : Quan<Q>> Q.deca(f: Q.() -> N) = f().d * 1E-1
+fun <Q : Quan<Q>> Number.deca(f: Double.() -> Q) = f(d * 1E1)
+fun <Q : Quan<Q>> Q.deca(f: Q.() -> Double) = f() * 1E-1
 
-fun <N : Number, Q : Quan<Q>> N.deci(f: Number.() -> Q) = f(d * 1E-1)
-fun <N : Number, Q : Quan<Q>> Q.deci(f: Q.() -> N) = f().d * 1E1
+fun <Q : Quan<Q>> Number.deci(f: Double.() -> Q) = f(d * 1E-1)
+fun <Q : Quan<Q>> Q.deci(f: Q.() -> Double) = f() * 1E1
 
-fun <N : Number, Q : Quan<Q>> N.centi(f: Number.() -> Q) = f(d * 1E-2)
-fun <N : Number, Q : Quan<Q>> Q.centi(f: Q.() -> N) = f().d * 1E2
+fun <Q : Quan<Q>> Number.centi(f: Double.() -> Q) = f(d * 1E-2)
+fun <Q : Quan<Q>> Q.centi(f: Q.() -> Double) = f() * 1E2
 
-fun <N : Number, Q : Quan<Q>> N.milli(f: Number.() -> Q) = f(d * 1E-3)
-fun <N : Number, Q : Quan<Q>> Q.milli(f: Q.() -> N) = f().d * 1E3
+fun <Q : Quan<Q>> Number.milli(f: Double.() -> Q) = f(d * 1E-3)
+fun <Q : Quan<Q>> Q.milli(f: Q.() -> Double) = f() * 1E3
 
-fun <N : Number, Q : Quan<Q>> N.micro(f: Number.() -> Q) = f(d * 1E-6)
-fun <N : Number, Q : Quan<Q>> Q.micro(f: Q.() -> N) = f().d * 1E6
+fun <Q : Quan<Q>> Number.micro(f: Double.() -> Q) = f(d * 1E-6)
+fun <Q : Quan<Q>> Q.micro(f: Q.() -> Double) = f() * 1E6
 
-fun <N : Number, Q : Quan<Q>> N.nano(f: Number.() -> Q) = f(d * 1E-9)
-fun <N : Number, Q : Quan<Q>> Q.nano(f: Q.() -> N) = f().d * 1E9
+fun <Q : Quan<Q>> Number.nano(f: Double.() -> Q) = f(d * 1E-9)
+fun <Q : Quan<Q>> Q.nano(f: Q.() -> Double) = f() * 1E9
 
-fun <N : Number, Q : Quan<Q>> N.pico(f: Number.() -> Q) = f(d * 1E-12)
-fun <N : Number, Q : Quan<Q>> Q.pico(f: Q.() -> N) = f().d * 1E12
+fun <Q : Quan<Q>> Number.pico(f: Double.() -> Q) = f(d * 1E-12)
+fun <Q : Quan<Q>> Q.pico(f: Q.() -> Double) = f() * 1E12
 
-fun <N : Number, Q : Quan<Q>> N.femto(f: Number.() -> Q) = f(d * 1E-15)
-fun <N : Number, Q : Quan<Q>> Q.femto(f: Q.() -> N) = f().d * 1E15
+fun <Q : Quan<Q>> Number.femto(f: Double.() -> Q) = f(d * 1E-15)
+fun <Q : Quan<Q>> Q.femto(f: Q.() -> Double) = f() * 1E15
 
-fun <N : Number, Q : Quan<Q>> N.atto(f: Number.() -> Q) = f(d * 1E-18)
-fun <N : Number, Q : Quan<Q>> Q.atto(f: Q.() -> N) = f().d * 1E18
+fun <Q : Quan<Q>> Number.atto(f: Double.() -> Q) = f(d * 1E-18)
+fun <Q : Quan<Q>> Q.atto(f: Q.() -> Double) = f() * 1E18
