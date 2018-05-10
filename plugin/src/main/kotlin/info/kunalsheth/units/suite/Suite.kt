c0f0f3ba5f2e7c2d1260c282e1769d836844f19f
Kunal Sheth <kunal@kunalsheth.info>
2018-05-10 04:10:44 -0700

    fix serialization exception on Suite objects

--- plugin/src/main/kotlin/info/kunalsheth/units/suite/Suite.kt
+++ plugin/src/main/kotlin/info/kunalsheth/units/suite/Suite.kt
@@ -2,19 +2,20 @@ package info.kunalsheth.units.suite
 
 import info.kunalsheth.units.data.Quantity
 import info.kunalsheth.units.data.UnitOfMeasure
-
+import java.io.Serializable
 
 sealed class Suite {
     abstract val units: Set<UnitOfMeasure>
     abstract val quantities: Set<Quantity>
 }
 
-class Squants(useUnits: Boolean = true, useQuantities: Boolean = true) : Suite() {
-    override val units = if(useUnits) SquantsExport.units else emptySet()
-    override val quantities = if(useQuantities) SquantsExport.quantities else emptySet()
+class Squants(private val useUnits: Boolean = true, private val useQuantities: Boolean = true) : Suite(), Serializable {
+    override val units get() = if (useUnits) SquantsExport.units else emptySet()
+    override val quantities = if (useQuantities) SquantsExport.quantities else emptySet()
 
 }
-class SI(useUnits: Boolean = true, useQuantities: Boolean) : Suite() {
-    override val units = if(useUnits) InternationalSystemOfUnits.units else emptySet()
-    override val quantities = if(useQuantities) InternationalSystemOfUnits.quantities else emptySet()
+
+class SI(private val useUnits: Boolean = true, private val useQuantities: Boolean) : Suite(), Serializable {
+    override val units get() = if (useUnits) InternationalSystemOfUnits.units else emptySet()
+    override val quantities get() = if (useQuantities) InternationalSystemOfUnits.quantities else emptySet()
 }
